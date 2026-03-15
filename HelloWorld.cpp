29ec9436a220414dccac0d72ab5633f4e6a65286
houdini18rgb <hudanana2023@gmail.com>
2026-03-14 19:42:22 -0700

    Add HelloWorld.cpp to display a greeting message
    This program displays a simple 'Hello World' message to demonstrate programming competency.

---
+++ HelloWorld.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Author: Huda Abdullah
+ * Date: 3/14/26
+ * Purpose: Display hello world (Show I'm a competent programmer) 
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) 
+{
+    //Set Random Number Seed Here
+    //Declare all Variables Here
+    //Input or initialize values Here
+    //Process/Calculations Here
+    cout << "\n";
+        cout << "   Hello World!  " << endl;
+        cout << "\n";
+    //Output Located Here
+    
+    //Exit
+    return 0;
+}
